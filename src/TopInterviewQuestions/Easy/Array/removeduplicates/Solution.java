8affa2041b8ee9d755006fc1e4ed090793678541
Petr Pichugin <petr.pichugin@gmail.com>
2022-02-24 14:39:01 +0500

    Package names corrected

--- src/TopInterviewQuestions/Easy/Array/removeduplicates/Solution.java
+++ src/TopInterviewQuestions/Easy/Array/removeduplicates/Solution.java
@@ -1,4 +1,4 @@
-package topinterviewquestions.easy.removeduplicates;
+package topinterviewquestions.easy.array.removeduplicates;
 
 /* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place
  * such that each unique element appears only once. The relative order of the elements should be kept the same.
--- src/TopInterviewQuestions/Easy/Array/twosum/Solution.java
+++ src/TopInterviewQuestions/Easy/Array/twosum/Solution.java
@@ -1,4 +1,4 @@
-package topinterviewquestions.easy.array;
+package topinterviewquestions.easy.array.twosum;
 
 /* Given an array of integers nums and an integer target,
  * return indices of the two numbers such that they add up to target.
